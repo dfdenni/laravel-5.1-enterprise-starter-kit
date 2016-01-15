bfd7b7e2ecd700b38679a45c36260908bf52e665
Dwi Fahni Denni <dfdenni0912@gmail.com>
2016-01-15 13:48:30 +0700

    Update Migration Fileupload
    Update structure for migration database “fileupload”

--- database/migrations/2016_01_14_050206_create_fileupload_table.php
+++ database/migrations/2016_01_14_050206_create_fileupload_table.php
@@ -17,16 +17,25 @@ class CreateFileuploadTable extends Migration
             $table->string('client_name');
             $table->string('client_id');
             $table->unsignedbigInteger('user_id')->nullable()->default(null);
+            $table->unsignedbigInteger('role_id')->nullable()->default(null);
             $table->string('path');
+            $table->string('filename');
+            $table->string('password_file');
             $table->string('meta_title')->nullable();
             $table->string('meta_description')->nullable();
             $table->string('meta_tag')->nullable();
+            $table->integer('status')->default(0);
             $table->timestamps();
 
             $table->foreign('user_id')
                   ->references('id')
                   ->on('users')
                   ->onDelete('cascade');
+
+            $table->foreign('role_id')
+                  ->references('id')
+                  ->on('roles')
+                  ->onDelete('cascade');      
  
         });
     }
