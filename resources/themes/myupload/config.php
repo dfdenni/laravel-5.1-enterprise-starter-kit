3ff84d87fc4b7f8873e9ed811d8402435f8f8236
Dwi Fahni Denni <dfdenni0912@gmail.com>
2016-01-16 19:40:03 +0700

    Define "myapp" config theme
    Add definition “myapp” config.php theme

--- resources/themes/myupload/config.php
+++ resources/themes/myupload/config.php
@@ -24,5 +24,11 @@ return array(
 
     'inherit' => null,
 
-
+    'myapp' => [
+            'app_name'       => env('APP_NAME', 'UPLoader'),
+            'app_desc'       => env('APP_DESCRIPTION', 'Laravel 5.1 Simple Uploader'),
+            'app_ver'        => env('APP_VERSION', '1.0.1'),
+            'app_copyright'  => env('APP_COPYRIGHT', '&copy; 2016'),
+            'app_company'    => env('APP_COMPANY', 'DFDenni'),
+        ],
 );
