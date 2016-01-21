da9ca0f8d6e124195a82381ca3d2737b8f453b0c
Dwi Fahni Denni <dfdenni0912@gmail.com>
2016-01-21 23:30:59 +0700

    Fixing path Uploader "Storage" Folder

--- config/settings.php
+++ config/settings.php
@@ -2,13 +2,13 @@
 
 return array(
 	'script_url' => URL::to_route('upload'),
-	'upload_dir' => path('storage').'uploads/files/',
-	'upload_url' => URL::base().'uploads/files/',
+	'upload_dir' => path('storage').'storage/uploads/files/',
+	'upload_url' => URL::base().'storage/uploads/files/',
 	'delete_type' => 'POST',
 	'image_versions' => array(
 		'thumbnail' => array(
-			'upload_dir' => path('storage').'uploads/thumbnails/',
-			'upload_url' => URL::base().'uploads/thumbnails/',
+			'upload_dir' => path('storage').'storage/uploads/thumbnails/',
+			'upload_url' => URL::base().'storage/uploads/thumbnails/',
 		),
 	),
 );
